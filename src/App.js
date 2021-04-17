ed18eefc71d17a6993bb8455f3b440d69f1aa5d7
fida ghanmi <fidaghanmi@gmail.com>
2021-04-17 10:14:03 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import pizza from "./pizza.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div style={{border:'solid 1px black', maxWidth:'100vw'}}/>
+      <h1 className="title red">Fida Ghanmi</h1>
+      <h3 className="title red">Menu</h3>
+      <br/>
+      
+<img src={pizza} height='250px' width='500px' alt='pizza' />
+
+<br/>
+
+<img src="/mlawi.jpg" height='250px' width='500px' alt='mlawi'/>
+
+<br/>
+
+<iframe width="500px" height="250px" src="https://www.youtube.com/embed/sv3TXMSv6Lw" 
+title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>
+        
+      
     </div>
   );
 }
